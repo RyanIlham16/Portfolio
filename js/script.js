90b88868ba96414d756dad1a032b403c2206bf0e
RyanIlham16 <42529439+RyanIlham16@users.noreply.github.com>
2024-11-30 17:01:14 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('a[href="#projects"]').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+});
